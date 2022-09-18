1816e9772b155a872ff732032b913e674ffc3dc4
Michael <mikhail.v.rogachev@gmail.com>
2022-09-18 16:42:06 +0500

    readlineSync

---
+++ src/cli.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync';
+const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name?: ');
+console.log('Hello, ' + name + '!');
+export {name};
