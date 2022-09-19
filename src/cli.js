23965ebe932cd81714fc2aac73de90d5593fa86b
Michael <mikhail.v.rogachev@gmail.com>
2022-09-19 12:19:09 +0500

    eslint.yml,cli.js-export defoult

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+
 const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name?: ');
 console.log('Hello, ' + name + '!');
-export {name};
+export default name;
