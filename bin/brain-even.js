311864c73b5ab7ff95968026b6e59ec80b475580
Michael <mikhail.v.rogachev@gmail.com>
2022-09-20 12:45:48 +0500

    file brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name?: ');
+console.log('Hello, ' + name + '!\nAnswer "yes" if the number is even, otherwise answer "no".');
+for (let i = 1; i <= 3; i += 1){
+    let number = Math.random() * 100;
+        number = Math.floor(number);
+        let value = '';
+        if (number % 2 === 0) {
+            value = 'yes';
+        }else {
+            value = 'no'
+        }
+    const value2 = readlineSync.question('Question:' + ' ' + number + '\nYour answer: ');
+           if (value2 === value) {
+            console.log('Correct!');
+           }else {
+            console.log("'yes' is wrong answer ;(. Correct answer was " + "'" + value + "'" + ".\nLet's try again, Bill!");
+            break
+           }
+}
