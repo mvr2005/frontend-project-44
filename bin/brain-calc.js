d0bc15013909e2b419fbf5f794d4bcbf183a291f
Michael <mikhail.v.rogachev@gmail.com>
2022-09-25 15:52:00 +0500

    brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import startGame from '../src/index.js';
+import { startGame } from '../src/index.js';
+import { text, array } from '../src/games/calc.js';
 
-startGame();
-console.log('hello');
+startGame(text, array);
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,5 +1,52 @@
-import { startGame } from '../index.js';
+import { selectNumber } from '../index.js';
+
+const selectOperanion = () => {
+  const operation = ['+', '-', '*'];
+  let selectedOperator = '';
+  const digit = selectNumber();
+  if (digit <= 33) {
+    selectedOperator = operation[0];
+  } else if (digit > 66) {
+    selectedOperator = operation[1];
+  } else {
+    selectedOperator = operation[2];
+  }
+  return selectedOperator;
+};
+
+const implementationGame = () => {
+  const array = [];
+  for (let i = 0; i < 6; i += 2) {
+    const digit1 = selectNumber();
+    const digit2 = selectNumber();
+    const operation = selectOperanion();
+    let actualValue = '';
+    let result = 0;
+    switch (operation) {
+      case '+':
+        result = digit1 + digit2;
+        result = String(result);
+        actualValue = `${digit1}${'+'}${digit2}`;
+        break;
+      case '-':
+        result = digit1 - digit2;
+        result = String(result);
+        actualValue = `${digit1}${'-'}${digit2}`;
+        break;
+      case '*':
+        result = digit1 * digit2;
+        result = String(result);
+        actualValue = `${digit1}${'*'}${digit2}`;
+        break;
+        // no default
+    }
+    array[i] = actualValue;
+    array[i + 1] = result;
+  }
+  return array;
+};
 
 const text = 'What is the result of the expression?';
+const array = implementationGame();
 
-startGame(text);
+export { text, array };
