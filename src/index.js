f8d02d4a8ede6b34032a8053ebed0e6c84c5b8be
Michael <mikhail.v.rogachev@gmail.com>
2022-10-02 12:31:58 +0500

    index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ export const startGame = (text, array) => {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`${'Hello, '}${name}${'!'}`);
   console.log(text);
+  let numberCorrectAnswers = 0;
   for (let i = 0; i < 6; i += 2) {
     const currentValue = readlineSync.question(`${'Question: '}${array[i]}${'\nYour answer: '}`);
     if (currentValue === array[i + 1]) {
       console.log('Correct!');
+      numberCorrectAnswers += 1;
+      if (numberCorrectAnswers === 3) {
+        console.log(`${'Congratulations,'} ${name}`);
+      }
     } else {
       console.log(`${"'"}${currentValue}${"'"}${"is wrong answer ;(. Correct answer was '"}${array[i + 1]}${"'.\nLet's try again, "}${name}${'!'}`);
       break;
