541f3303ada95608d7315a21d71fd96718d6ecc6
Michael <mikhail.v.rogachev@gmail.com>
2022-10-02 13:13:17 +0500

    text prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -17,7 +17,7 @@ const implementationGame = () => {
   return array;
 };
 
-const text = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const text = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 const array = implementationGame();
 
 export { text, array };
