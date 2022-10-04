bc48068f68753dd2c7eb0b393950e4fb02c73877
Michael <mikhail.v.rogachev@gmail.com>
2022-10-04 16:12:28 +0500

    calc.js + calculation

--- src/games/calc.js
+++ src/games/calc.js
@@ -20,29 +20,32 @@ const selectOperanion = () => {
   return selectedOperator;
 };
 
+const calculation = (digit1, digit2, operation) => {
+  let result = 0;
+  switch (operation) {
+    case '+':
+      result = digit1 + digit2;
+      break;
+    case '-':
+      result = digit1 - digit2;
+      break;
+    case '*':
+      result = digit1 * digit2;
+      break;
+      // no default
+  }
+  return result;
+};
+
 const implementationGame = () => {
   const arrayTaskSolution = [];
   for (let i = 0; i < 3; i += 1) {
-    const digit1 = generateRandomNumber(1, 100); const digit2 = generateRandomNumber(1, 100);
+    const digit1 = generateRandomNumber(1, 100);
+    const digit2 = generateRandomNumber(1, 100);
     const operation = selectOperanion();
-    let actualValue = ''; let result = 0;
-    switch (operation) {
-      case '+':
-        result = digit1 + digit2;
-        actualValue = `${digit1} ${'+'} ${digit2}`;
-        break;
-      case '-':
-        result = digit1 - digit2;
-        actualValue = `${digit1} ${'-'} ${digit2}`;
-        break;
-      case '*':
-        result = digit1 * digit2;
-        actualValue = `${digit1} ${'*'} ${digit2}`;
-        break;
-        // no default
-    }
-    result = String(result);
-    arrayTaskSolution[i] = [actualValue, result];
+    const actualValue = `${digit1} ${operation} ${digit2}`;
+    const result = calculation(digit1, digit2, operation);
+    arrayTaskSolution[i] = [actualValue, String(result)];
   }
   return arrayTaskSolution;
 };
