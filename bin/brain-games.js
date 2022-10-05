04b38ded763d096eaca6c625b5c7811e898e656c
Michael <mikhail.v.rogachev@gmail.com>
2022-10-05 17:31:44 +0500

    correction brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
+import askQuestion from '../src/cli.js';
+
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
 };
+
+greeting();
+askQuestion();
 
 export default greeting;
