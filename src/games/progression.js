99e3bb0b5b585818699c1343dce02ddf4960566b
Michael <mikhail.v.rogachev@gmail.com>
2022-10-06 14:51:21 +0500

    progression.js correction

--- src/games/progression.js
+++ src/games/progression.js
@@ -11,17 +11,22 @@ const progressionString = (arr) => {
   return progressionStrings;
 };
 
+const progression = () => {
+  const stepProgession = generateRandomNumber(1, 20);
+  const startProgression = generateRandomNumber(1, 20);
+  const arrProgression = [startProgression];
+  for (let j = 0; j < 9; j += 1) {
+    arrProgression.push(arrProgression[j] + stepProgession);
+  }
+  return arrProgression;
+};
+
 const implementationGame = () => {
   const arrayTaskSolution = [];
   let digit = 0;
   for (let i = 0; i < numberRound(); i += 1) {
-    const stepProgession = generateRandomNumber(1, 20);
     const passPlace = generateRandomNumber(1, 10);
-    const startProgression = generateRandomNumber(1, 20);
-    const arrProgression = [startProgression];
-    for (let j = 0; j < 9; j += 1) {
-      arrProgression.push(arrProgression[j] + stepProgession);
-    }
+    const arrProgression = progression();
     digit = String(arrProgression[passPlace]);
     arrProgression[passPlace] = '..';
     arrayTaskSolution[i] = [progressionString(arrProgression).trim(), digit];
