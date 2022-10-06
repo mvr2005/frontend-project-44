c45a281172217da2e963858660dc232648f30218
Michael <mikhail.v.rogachev@gmail.com>
2022-10-06 16:14:15 +0500

    prime.js correction

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,17 +2,22 @@ import startGame from '../index.js';
 import generateRandomNumber from '../generate-random-number.js';
 import numberRound from '../number-round.js';
 
+const primeNumberCheck = (actualValue) => {
+  let value = 'yes';
+  for (let i = 2; i < actualValue; i += 1) {
+    if (actualValue % i === 0) {
+      value = 'no';
+      break;
+    }
+  }
+  return value;
+};
+
 const implementationGame = () => {
   const arrayTaskSolution = [];
   for (let i = 0; i < numberRound(); i += 1) {
-    let value = 'yes';
     const actualValue = generateRandomNumber(2, 100);
-    for (let j = 2; j < actualValue; j += 1) {
-      if (actualValue % j === 0) {
-        value = 'no';
-        break;
-      }
-    }
+    const value = primeNumberCheck(actualValue);
     arrayTaskSolution[i] = [actualValue, value];
   }
   return arrayTaskSolution;
