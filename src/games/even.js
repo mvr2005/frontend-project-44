8690e047d38aae65ff42193680c32c1db6e957bc
Michael <mikhail.v.rogachev@gmail.com>
2022-10-06 16:05:44 +0500

    even.js correction

--- src/games/even.js
+++ src/games/even.js
@@ -2,16 +2,21 @@ import startGame from '../index.js';
 import generateRandomNumber from '../generate-random-number.js';
 import numberRound from '../number-round.js';
 
+const parityCheck = (actualValue) => {
+  let value = '';
+  if (actualValue % 2 === 0) {
+    value = 'yes';
+  } else {
+    value = 'no';
+  }
+  return value;
+};
+
 const implementationGame = () => {
   const arrayTaskSolution = [];
   for (let i = 0; i < numberRound(); i += 1) {
     const actualValue = generateRandomNumber(1, 100);
-    let value = '';
-    if (actualValue % 2 === 0) {
-      value = 'yes';
-    } else {
-      value = 'no';
-    }
+    const value = parityCheck(actualValue);
     arrayTaskSolution[i] = [actualValue, value];
   }
   return arrayTaskSolution;
