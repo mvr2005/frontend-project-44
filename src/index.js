063251ef274a12001010e25e5d46a889fd9a48cd
Michael <mikhail.v.rogachev@gmail.com>
2022-10-08 16:24:06 +0500

    correction index.js

--- src/index.js
+++ src/index.js
@@ -14,15 +14,16 @@ export const startGame = (textTask, arrayTaskSolution) => {
   console.log(textTask);
   let numberCorrectAnswers = 0;
   for (let i = 0; i < numberRound; i += 1) {
-    const currentValue = readlineSync.question(`${'Question: '}${arrayTaskSolution[i][0]}${'\nYour answer: '}`);
+    const currentValue = readlineSync.question(`Question: ${arrayTaskSolution[i][0]}\nYour answer: `);
     if (currentValue === arrayTaskSolution[i][1]) {
       console.log('Correct!');
       numberCorrectAnswers += 1;
       if (numberCorrectAnswers === 3) {
-        console.log(`${'Congratulations,'} ${name}!`);
+        console.log(`Congratulations, ${name}!`);
       }
     } else {
-      console.log(`${"'"}${currentValue}${"'"}${"is wrong answer ;(. Correct answer was '"}${arrayTaskSolution[i][1]}${"'.\nLet's try again, "}${name}${'!'}`);
+      console.log(`'${currentValue}'is wrong answer ;(. Correct answer was '${arrayTaskSolution[i][1]}'.`);
+      console.log(`Let's try again, ${name}!`);
       break;
     }
   }
