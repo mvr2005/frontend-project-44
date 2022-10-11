20e4750ee2234c65babba27aec17036e1fd3195a
Michael <mikhail.v.rogachev@gmail.com>
2022-10-11 12:28:12 +0500

    index.js delete numberCorrectAnswers

--- src/index.js
+++ src/index.js
@@ -12,15 +12,13 @@ export const numberRound = 3;
 export const startGame = (textTask, arrayTaskSolution) => {
   const name = startGames();
   console.log(textTask);
-  let numberCorrectAnswers = 0;
   for (let i = 0; i < numberRound; i += 1) {
     const task = arrayTaskSolution[i][0];
     const answer = arrayTaskSolution[i][1];
     const currentValue = readlineSync.question(`Question: ${task}\nYour answer: `);
     if (currentValue === answer) {
       console.log('Correct!');
-      numberCorrectAnswers += 1;
-      if (numberCorrectAnswers === 3) {
+      if (i === numberRound - 1) {
         console.log(`Congratulations, ${name}!`);
       }
     } else {
