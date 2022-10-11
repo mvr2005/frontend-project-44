abb2a4a1f6c30056a61b08d8371cc5fec3987e4b
Michael <mikhail.v.rogachev@gmail.com>
2022-10-11 09:41:34 +0500

    destructuring index.js

--- src/index.js
+++ src/index.js
@@ -14,15 +14,17 @@ export const startGame = (textTask, arrayTaskSolution) => {
   console.log(textTask);
   let numberCorrectAnswers = 0;
   for (let i = 0; i < numberRound; i += 1) {
-    const currentValue = readlineSync.question(`Question: ${arrayTaskSolution[i][0]}\nYour answer: `);
-    if (currentValue === arrayTaskSolution[i][1]) {
+    const task = arrayTaskSolution[i][0];
+    const answer = arrayTaskSolution[i][1];
+    const currentValue = readlineSync.question(`Question: ${task}\nYour answer: `);
+    if (currentValue === answer) {
       console.log('Correct!');
       numberCorrectAnswers += 1;
       if (numberCorrectAnswers === 3) {
         console.log(`Congratulations, ${name}!`);
       }
     } else {
-      console.log(`'${currentValue}'is wrong answer ;(. Correct answer was '${arrayTaskSolution[i][1]}'.`);
+      console.log(`'${currentValue}'is wrong answer ;(. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${name}!`);
       break;
     }
