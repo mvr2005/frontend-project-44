810c5b5b6d1d883bb28db1027275d68c6a3c5b5b
Michael <mikhail.v.rogachev@gmail.com>
2022-10-26 13:47:54 +0500

    correction cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,5 @@ const startGame = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
-  return name;
 };
 export default startGame;
