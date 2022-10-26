652a227e3402ee0943a8e9379ba0c2f7ad1e7955
Michael <mikhail.v.rogachev@gmail.com>
2022-10-26 13:43:54 +0500

    rename askQuestion

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import askQuestion from '../src/cli.js';
+import startGame from '../src/cli.js';
 
-askQuestion();
+startGame();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const askQuestion = () => {
+const startGame = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`${'Hello, '}${name}${'!'}`);
   return name;
 };
-export default askQuestion;
+export default startGame;
